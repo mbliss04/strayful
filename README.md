f44ab37f49196c69ada2b4aa10adbd750b8f0952
McCall Bliss <missmccallkay@gmail.com>
2013-08-13 21:05:45 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+stray
+=====
+author: McCall Bliss
+created: August 2013
+
+All documents for the Stray website.
---
+++ js/pageload.js
@@ -0,0 +1,23 @@
+/*
+      .-      .-                      .-:::-.`                        
+     `NM:    .MM.                     yMMNNMMMmo`                     
+     +MMd`   hMMs   .--.`  `..     ...yMM.  .sMMs `--.`     .--.`+.   
+     dMMMo  /MMMN` yNmmNMm+-mMd- `yMN/yMM.   :MMysNmmNMm+`sNMmmMMMy`  
+    -MMdMN.`mMdMM/ ``.-:yMM-`sMMyNMh. yMMsooyNMm.``.--sMMmMN:  :MMy   
+    yMM`yMhyMm`mMd:hNNmdmMM/  /MMMs   yMMdhhyo: .yNNmdmMMNMMo``+MMs   
+   `NMy `mMMN- +MMMMh`  /MM/ /NMmMMs` yMM.      NMd`  :MMooMMMMMd+`   
+   +MM:  :MM+  .NMMMNo+sNMMsyMN+ -mMm:yMM.      dMNs+smMMmMMdoyyso-   
+   +so    +o    +so+syso-/ysso.   `oso+so`       /syyo-:ys+oso++hMM:  
+                                                         .dms++odMN.  
+                                                         `/oyhhhs/` 
+														 
+	jQuery PageLoading
+	Version : 1.0
+
+	Author : PAGNOULLE Maxime (MaxPag)
+	http://maxpag.eu
+	http://codecanyon.net/user/MaxPag/
+ 
+***************************************************************************/
+
+eval((function(x){var d="";var p=0;while(p<x.length){if(x.charAt(p)!="`")d+=x.charAt(p++);else{var l=x.charCodeAt(p+3)-28;if(l>4)d+=d.substr(d.length-x.charCodeAt(p+1)*96-x.charCodeAt(p+2)+3104-l,l);else d+="`";p+=4}}return d})("jQuery.PadeLoading = function (Options) {var Settings = {Delay:0, BgIMG:\"\", BgColor:\"#000000` .!Inner` 0$FFFFFF\", Bord` -&` F%Shadow` 0#\", Loader` {!` \"%.gif\", zIndex:99};if`!]($.extend(`!f$, ` 5$;}`\"1%Center` l%(DivTo` /\"`\"F#IMG = new Image;` -#Src =`\"a%.` Y%;$(IMG).load(`#7&` f#posTopOffset, posLeft` )\" = \"\";` 5( = \"-\" + (IMG.height / 2 + 15) + \"px` M!` W*` I&width` @.$`\"9).css({position:\"fixed\", left:\"50%\", top` #$'margin-top':`\"!*` 3$left` 7!`!N&, 'z-index':`#+%`$U\" + 1});delete IMG;}).error`#1*`!,f0`![,0`!=>}).attr({src:`%9\"}`%|(`(N#Page() {if `&O%`%R& != \"\"`\"'!\"body\").append(\"<div id=\\\"OverMask\\\"></div>` 1&` c\"Div\\\"><img src=\\\"\" +`&V/ + \"` [%\")`(9!` =%`)h! =`!E&#`!/$\"`#T#`%w!:\"10`#;!`&W\"` &%'background-color`#,'`*W#`'Y!`$6#absolute`$8$0`$6\"0` 8(`$Y$`#p5}).fadeTo(\"slow\", 0.7);} else`!E[image':\"url(`#A)`#\"\"+ \")\"`!/~`\"*#`,#,$(\"#`%1%\"))`+}!Red, Green, Blue`$p+`.-&`&K%Red = HexToRe`-L&` @();` m!` ?$` (!` 23Blue` @$Blue` 03`$6$`!7\"0` w%0` Y$0;}`!m)`0j! != 0`&q#`\"O'.hide(`$u#`&E:`1$&, '-moz-` C(lip':\"padding\", '-webkit` ':` $5-box\", b`\"T!:\"10px solid rgba`&,!Red + \", \" +`$Q\"` %&`\"e!` )!0.3)`!B)` m!-radius':\"13px` ;!`\",!` &3` <5` l%x-s`3[!':\"0px 0px 5px`'k)`3v'`!&&` 'M` YN`#K#:\"5px\"`(:#In`(:#).delay`%^+` ?\"Out`(^%`0H+`*k)` D$);}`+.*`%F~`%F~`%F~`%F~`%F~`&&>;}`%R$alert(\"The `3O&s` m\" can not be empty !` V!`&V%`.C%Value) {return parseInt(CutDiese` :#.substring(0, 2), 16);` a+`.o\"` @N2, 4` \\2`/:!` ?N4, 6` `-` @+` g%` *!.charAt(0) == \"#\" ?` 2#` l&1, 7) :` 2\";}$(`* )LoadingPage`){#;jQuery.Pade`$=!d =`*O.`*1(`*TV;"))
