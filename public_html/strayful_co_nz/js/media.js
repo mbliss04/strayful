d4dc403c83edfcb7b86b7330720d69f32177fe77
McCall Bliss <missmccallkay@gmail.com>
2013-09-28 19:37:57 +1200

    update margins

--- public_html/strayful_co_nz/js/media.js
+++ public_html/strayful_co_nz/js/media.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 	$('.directorySlider').directorySlider();
 
+	/* Resize widths on window change */
 	$( window ).resize(function() {
 		$('directorySlider').resizeWidths();
+		$('gallery-options li').css({'margin-right':+-1});
 	});
+
+
+
+	/* Arrows for gallery scroll */
 
 	timer = setTimeout("$('.scroll').fadeOut(1500);", 1200);
 
--- public_html/strayful_co_nz/js/values.js
+++ public_html/strayful_co_nz/js/values.js
@@ -22,16 +22,16 @@ $(document).ready(function(){
 	function information(id, easing, option) {
 		$(".diamond").fadeTo(easing, option);
 		if (element_id == "fire") {
-			$(".description").html('<h2>Fire</h2><div class="info"><p>We feel that everyone has a secret passion ruling their heart, a spark kindling their bones, and a little bit of curiosity that wants to light the match.</p></div>').fadeTo(easing, option);
+			$(".description").html('<h2>Fire</h2><div class="info"><p>There lies a secret passion in all of us: an ember smouldering in our bones waiting to be set alight. Stray is the stoker, poking and prodding to bring that creativity to a blaze with a bold and lively style.</p></div>').fadeTo(easing, option);
 		}
 		if (element_id == "air") {
-			$(".description").html('<h2>Air</h2><div class="info"><p>The oxygen </p></div>').fadeTo(easing, option);
+			$(".description").html('<h2>Air</h2><div class="info"><p>The sky is our limit, freedom, adventure, and discovery: our life’s blood. To us there is no fear of the unknown; we’ll try anything once or thrice. Stray isn’t fixed to any one genre of design; it is hindered only by the imagination.</p></div>').fadeTo(easing, option);
 		}
 		if (element_id == "earth") {
-			$(".description").html('<h2>Earth</h2><div class="info"><p></p></div>').fadeTo(easing, option);
+			$(".description").html('<h2>Earth</h2><div class="info"><p>In every being there is a longing for connection and unity, a need to help those that surround us. We’re going back to our roots making sure everything we make is a local, handcrafted affair. After all, home is where the heart is.</p></div>').fadeTo(easing, option);
 		}
 		if (element_id == "water") {
-			$(".description").html('<h2>Water</h2><div class="info"><p></p></div>').fadeTo(easing, option);
+			$(".description").html('<h2>Water</h2><div class="info"><p>The river of change is constantly flowing, and we sail it with gusto. We believe that green design shouldn’t be a fad but a design standard. In the spirit of kiwi ingenuity, Stray sees gold where others see waste.</p></div>').fadeTo(easing, option);
 		}
 	}
 });
