9dbf5344bb609e7d6b8fe045c1c3cca101561ab4
McCall Bliss <missmccallkay@gmail.com>
2013-10-03 16:56:15 +1300

    resize media for media page

---
+++ public_html/strayful_co_nz/js/videos.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  // Find all YouTube/Vimeo videos
+  var $allVideos = $("iframe[src^='http://player.vimeo.com'], iframe[src^='http://www.youtube.com']"),
+
+  // The element that is fluid width
+  $fluidEl = $(".container .videos .leftcol");
+
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function() {
+
+    $(this)
+      .data('aspectRatio', this.height / this.width)
+
+      // and remove the hard coded width/height
+      .removeAttr('height')
+      .removeAttr('width');
+
+  });
+
+  // When the window is resized
+  $(window).resize(function() {
+
+    console.log($fluidEl.width());
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
+
+      var $el = $(this);
+      $el
+        .width(newWidth)
+        .height(newWidth * $el.data('aspectRatio'));
+
+    });
+
+  // Kick off one resize to fix all videos on page load
+  }).resize();
+});
