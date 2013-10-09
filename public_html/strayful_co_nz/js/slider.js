ce99fb53cf46dcdc0e8a05882096544f1653a421
McCall Bliss <missmccallkay@gmail.com>
2013-10-10 11:11:46 +1300

    change sliding to do whole page

--- public_html/strayful_co_nz/js/slider.js
+++ public_html/strayful_co_nz/js/slider.js
@@ -4,21 +4,38 @@ $(document).ready(function(){
 	var rightbtn = $('.rightbtn');
 	var leftbtn = $('.leftbtn');
 	var gallery = $('.gallery-wrapper');
+	var numSlides = 5;
   	var slides = new Array();
+  	var toSwitch = new Array();
 
     //when user clicks the image for sliding right  
     rightbtn.click(function(){
-  		slides = $('.gallery-li').get();
-  		$('.gallery-li').get(0).remove();
-  		$('.gallery-ul').append(slides[0]);
+    	// get and remove first five li
+  		for(var i = 0; i < numSlides; i++) {
+  			toSwitch[i] = $('.gallery-li').get(0);
+  			$('.gallery-li').get(0).remove();
+  		}
+  		// add to the end of the list
+  		for (var i = 0; i < 5; i++) {
+  			$('.gallery-ul').append(toSwitch[i]);
+  		}
  	});  
   
     //when user clicks the image for sliding left  
     leftbtn.click(function(){
-  		slides = $('.gallery-li').get();
-  		var last_slide = slides.length - 1;
-  		$('.gallery-li').get(last_slide).remove();
-  		$('.gallery-ul').prepend(slides[last_slide]);
+    	// make array from all li and get last slide index
+    	slides = $('.gallery-li').get();
+    	var last_slide = slides.length - 1;
+    	// get and remove the last five li
+    	for (var i = 0; i < numSlides; i++) {
+    		toSwitch[i] = $('.gallery-li').get(last_slide);
+  			$('.gallery-li').get(last_slide).remove();
+  			last_slide--;
+    	}
+    	// add to the front of the list
+    	for (var i = 0; i < numSlides; i++) {
+  			$('.gallery-ul').prepend(toSwitch[i]);
+  		}
     });
 
 });
