5d7d3ac4dfcda5a64f4e108846046ea10ee5bed5
McCall Bliss <missmccallkay@gmail.com>
2013-10-29 20:06:55 +1300

    add a classid element

--- json_gallery.php
+++ json_gallery.php
@@ -10,11 +10,12 @@
 
 	$i = 0;
 	while ($file = readdir($dirHandle)) {
-		if (!(is_dir($file)) && strpos($file, '.jpg') && $file != 'filler.jpg') {
+		if (!(is_dir($file)) && strpos($file, '.jpg')) {
 			$i++;
 			$src = "$dir$file";
-			list($category, $number) = split('[_]', $file);
-			$jsonData .= '"img'.$i.'":{ "num":"'.$i.'","src":"'.$src.'", "url":"#", "name":"'.$file.'", "category":"'.$category.'" },';
+			list($category, $classid, $imgtype) = split('[_.]', $file);
+			$classid = str_replace(' ', '', $classid);
+			$jsonData .= '"img'.$i.'":{ "num":"'.$i.'","src":"'.$src.'", "url":"#", "name":"'.$file.'", "category":"'.$category.'", "classid":"'.$classid.'"},';
 		}
 	}
 
